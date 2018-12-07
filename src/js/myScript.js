9fb6bfbb4eb5ffc437154faae4c09396f92e834d
Michael Overgaard Nielsen <mich4863@edu.campusvejle.dk>
2018-12-07 12:03:05 +0100

    Cardio 3

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -283,6 +283,63 @@ function letterChanges()
   return document.getElementById("output11").innerHTML = result; 
 }
 
+
+/*----------------------------------- Session 3 ----------------------------------------*/ 
+
+// CHALLENGE 1: ADD ALL NUMBERS
+// Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
+// ex. addAll(2,5,6,7) === 20
+
+function addAll(...args) 
+{
+  let result = 0;
+
+  for(let arg of args)
+  {
+    result += arg;
+  }
+  return result;
+}
+
+// CHALLENGE 2: SUM ALL PRIMES
+// Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
+// ex. sumAllPrimes(10) == 17
+
+function isPrime(NumberToCheck)
+{
+  for(let i = 2; i < NumberToCheck; i++)
+  {
+    if(NumberToCheck % i === 0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+function sumAllPrimes(loopNumber) 
+{
+  let result = 0;
+
+  for (let i = 2; i <= loopNumber; i++)
+  {
+    if(isPrime(i))
+    {
+      result += i;
+    }
+  }
+  console.log(result);
+}
+
+// CHALLENGE 3: SEEK & DESTROY
+// Remove from the array whatever is in the following arguments. Return the leftover numbers in an array
+// ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
+
+function seekAndDestroy() 
+{
+  
+}
+
 // OBJECT CONSTRUCTOR
 /*
 function User(firstName, lastName)
